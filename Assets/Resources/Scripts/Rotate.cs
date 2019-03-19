02bf3c8c76ee765b7f2d22d9ebc4412b1dc74d10
Valeria <lera.googlit@gmail.com>
2019-03-19 23:59:38 +0300

    Final version
    Loading on github

---
+++ Assets/Resources/Scripts/Rotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+	//public float X, Y, Z;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+		if (Input.GetKey(KeyCode.Mouse0))
+		{
+			transform.Rotate(Vector3.right, Input.GetAxis("Mouse Y"), Space.World);
+            transform.Rotate(Vector3.down, Input.GetAxis("Mouse X"), Space.World);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Teleport : MonoBehaviour {
+
+	public bool teleported = false;
+	public Teleport target;
+	
+	void OnTriggerEnter (Collider other)
+	{
+		if (other.CompareTag("MainCamera"))
+		{
+			if (!teleported)
+			{
+				target.teleported = true;
+				other.transform.position = target.gameObject.transform.position;
+			}
+		}
+	}
+	
+	void OnTriggerExit (Collider other)
+	{
+		if(other.CompareTag("MainCamera"))
+		{
+			teleported = false;
+		}
+	}
+}
